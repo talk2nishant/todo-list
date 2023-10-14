929c8b3d4db5cc8e1f30c6f0e251e9f61caf2bc1
Nishant Kamboj <talk2nishantkamboj@gmail.com>
2023-10-14 17:54:05 +0530

    todo list app

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./util/db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const todomodel = require('./model/todo');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+// setup cors to allow requests from localhost:3000 only? 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Simple todo api');
+});
+
+// add new todo
+app.post('/todos', async (req, res) => {
+    try {
+        const { item } = req.body;
+
+        const newtodo = new todomodel({
+            item
+        });
+
+        await newtodo.save();
+
+        return res.json({
+            sucess: true,
+            todo: newtodo
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong."
+        })
+    }    
+});
+
+// get all todo list
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await todomodel.find();
+        return res.json({
+            sucess: true,
+            todos: todos
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong."
+        })
+    } 
+});
+
+// remove todo item
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await todomodel.findByIdAndDelete(id);
+        return res.json({
+            sucess: true
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong."
+        })
+    } 
+});
+
+// change todo status
+app.post('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+
+        await todomodel.findByIdAndUpdate(id, {
+            status
+        }, {
+            runValidators: true
+        });
+
+        const todo = await todomodel.findById(id);
+
+        return res.json({
+            sucess: true,
+            todo: todo
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
